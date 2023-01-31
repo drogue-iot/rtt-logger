021537f768e5f12e377765ebb4ef332793553cbe
Trevor Gross <tmgross@umich.edu>
2023-01-31 17:18:49 -0500

    Added example to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
+//! A logger implementation to use RTT with the Rust `log` crate.
+//!
+//! ```
+//! use log::{info, LevelFilter};
+//! use rtt_logger::RTTLogger;
+//! use rtt_target::rtt_init_print;
+//!
+//! // logger configuration
+//! const LOG_LEVEL: LevelFilter = LevelFilter::Info;
+//! static LOGGER: RTTLogger = RTTLogger::new(LOG_LEVEL);
+//!
+//! fn main() {
+//!     // logger setup
+//!     rtt_init_print!();
+//!     log::set_logger(&LOGGER)
+//!         .map(|()| log::set_max_level(LOG_LEVEL))
+//!         .unwrap();
+//!
+//!     // logger usage in main binary or in any library
+//!     info!("Hello World!");
+//! }
+//! ```
+
 #![no_std]
 
-use log::{Metadata, Record, LevelFilter};
+use log::{LevelFilter, Metadata, Record};
 
 use rtt_target::*;
 
@@ -14,9 +37,7 @@ impl RTTLogger {
     ///
     /// * `level_filter`: The default level to enable.
     pub const fn new(level_filter: LevelFilter) -> RTTLogger {
-        RTTLogger {
-            level_filter
-        }
+        RTTLogger { level_filter }
     }
 }
 
@@ -31,7 +52,5 @@ impl log::Log for RTTLogger {
         }
     }
 
-    fn flush(&self) {
-    }
+    fn flush(&self) {}
 }
-
